801774011b9d665f9af441af31c1ce3f33e3192d
Marina Alba Rodriguez Estebanez <117929211+MofMiq@users.noreply.github.com>
2024-03-09 22:42:54 +0100

    iconitos puestos

--- Assets/Scripts/DrawCard.cs
+++ Assets/Scripts/DrawCard.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class DrawCard : MonoBehaviour
 {
     public Button b1, b2, b3, b4;
+    public Animator respawnTrig;
+
     void Start()
     {
     }
@@ -24,23 +26,40 @@ public class DrawCard : MonoBehaviour
             return false;
     }
 
+    public void ChangeCardData(Button b)
+    {
+            TextMeshProUGUI text = b.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = "Ataque 5";
+            b.transform.localScale = new Vector3(1, 1, 1);
+            //respawnTrig.SetTrigger("Respawn");
+            b.gameObject.SetActive(true);
+    }
     public void DrawOneCard()
     {
         if (CheckActive(b1))
-        {
-            Debug.Log("En DrawCard");
-            TextMeshProUGUI text = b1.GetComponentInChildren<TextMeshProUGUI>();
-            text.text = "Ataque 5";
-            b1.transform.localScale = new Vector3(1, 1, 1);
-            b1.gameObject.SetActive(true);
-        }
+            ChangeCardData(b1);
         if (CheckActive(b2))
-        {
-            Debug.Log("En DrawCard");
-            TextMeshProUGUI text = b2.GetComponentInChildren<TextMeshProUGUI>();
-            text.text = "Ataque 5";
-            b2.gameObject.SetActive(true);
-        }
+            ChangeCardData(b2);
+        if (CheckActive(b3))
+            ChangeCardData(b3);
+        if (CheckActive(b4))
+            ChangeCardData(b4);
     }
+
+/*     IEnumerator ScaleUp()
+    {
+        float duration = 1.0f;
+        float start = 0.0f;
+        Vector3 initScale = new Vector3(0, 0, 0);
+        Vector3 finalScale = new Vector3(1, 1, 1);
+
+        while (start < duration)
+        {
+            b1.transform.localScale = Vector3.Lerp(initScale, finalScale, start / duration);
+            start += Time.deltaTime / duration;
+            yield return null;
+        }
+        b1.transform.localScale = finalScale;
+    } */
 
 }
