cf9cc6f72c8ead31419f973b882f966d22f85dc0
pablobn <pablobn64@gmail.com>
2024-03-10 20:58:35 +0100

    Fix merge errors

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -24,54 +24,6 @@ public class CardController : MonoBehaviour
         transform.localScale = new Vector3(1, 1, 1);
     }
 
-    public void PlayCard()
-    {
-        // Add to discard pile
-        TableController.Instance.AddToDiscardPile(cardData);
-
-        // Execute all card actions
-        // foreach (CardAction action in cardData.cardActions)
-        // {
-        //     if (action.executeIfStatusActive != null && action.executeIfStatusActive.Value <= 0)
-        //         continue; // Skip action if status is selected and not active
-        //     switch (action.actionType)
-        //     {
-        //         case CardAction.ActionType.damage:
-        //             // Deal damage
-        //             Debug.Log("Dealt " + action.amount.value + " damage");
-        //             break;
-        //         case CardAction.ActionType.heal:
-        //             // Heal
-        //             Debug.Log("Healed " + action.amount.value + " health");
-        //             break;
-        //         case CardAction.ActionType.block:
-        //             // Block
-        //             Debug.Log("Blocked " + action.amount.value + " damage");
-        //             break;
-        //         case CardAction.ActionType.drawRandomCard:
-        //             // Draw random card
-        //             Debug.Log("Drew " + action.amount.value + " cards");
-        //             break;
-        //         case CardAction.ActionType.recoverCard:
-        //             // Recover card
-        //             Debug.Log("Recovered " + action.amount.value + " cards");
-        //             break;
-        //         case CardAction.ActionType.discardRandomCard:
-        //             // Discard random card
-        //             Debug.Log("Discarded " + action.amount.value + " cards");
-        //             break;
-        //         case CardAction.ActionType.applyStatus:
-        //             // Apply status
-        //             Debug.Log("Applied status ");
-        //             break;
-        //     }
-        // }
-
-        // Deactivate card
-        TableController.Instance.DeactivateObject(gameObject);
-
-    }
-
     // public void DealDamage(CardAction action)
     // {
     //     float baseDamage = action.amount.value;
